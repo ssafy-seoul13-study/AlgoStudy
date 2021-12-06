2171f10ac15550283954fdb5dbc454d7298cbfc6
zzangbomi <53842993+zzangbomi@users.noreply.github.com>
2021-12-06 11:23:16 +0900

    잃어버린 괄호

---
+++ 1주차/이보미/BOJ_1541.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 잃어버린 괄호
+//메모리 : 14248KB, 시간 : 124ms, 풀이시간 : 30분
+public class BOJ_1541 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		String [] arr = str.split("\\-");
+		
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			String [] temp = arr[i].split("\\+");
+			int tempSum = 0;
+			
+			if(i == 0) {
+				for (int j = 0; j < temp.length; j++) {
+					tempSum += Integer.parseInt(temp[j]);
+				}
+				sum += tempSum;
+			} else {
+				for (int j = 0; j < temp.length; j++) {
+					tempSum += Integer.parseInt(temp[j]);
+				}
+				sum -= tempSum;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
