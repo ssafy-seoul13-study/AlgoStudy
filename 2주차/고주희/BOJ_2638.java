1451fa61a5abdf39155380aaf75dc7353a4f7617
ko509 <62974220+ko509@users.noreply.github.com>
2021-12-11 20:54:28 +0900

    치즈

--- 2주차/고주희/BOJ_2638.java
+++ 2주차/고주희/BOJ_2638.java
@@ -7,12 +7,13 @@ import java.util.StringTokenizer;
 
 // 시간 : 260ms
 // 메모리 : 44880KB
-// 풀이 시간 : 26
+// 풀이 시간 : 26분
 public class BOJ_2638 {
 
 	static int N;
 	public static void main(String[] args) throws IOException {
 
+		// 입력받기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -24,26 +25,36 @@ public class BOJ_2638 {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < C; j++) {
 				cheese[i][j] = Integer.parseInt(st.nextToken()); 
-                if(cheese[i][j]==1) N++;
+                		// 치즈 크기 카운트
+				if(cheese[i][j]==1) N++;
 			}
 		}
+		// 다 녹을 때까지 걸리는 시간
 		int time = 0;
+		// 치즈 크기가 0이 될 때까지
 		while(N!=0) {
+			// 치즈 녹이고 시간++
 			melting(cheese);
 			time++;
 		}
 		System.out.println(time);
 	}
 
+	// 치즈 녹이는 함수
 	private static void melting(int[][] cheese) {
 
+		// 필요한 변수들 정의
         int[] dr = {-1,0,1,0};
 	    int[] dc = {0,-1,0,1};
         int R = cheese.length;
         int C = cheese[0].length;
+		
+		// 몇 면이 닿아있는지 셀 임시 배열
 		int[][] temp = new int[R][C];
+		// 치즈가 없는 0인 부분을 bfs
 		Queue<int[]> que = new LinkedList<int[]>();
 		que.offer(new int[] {0,0});
+		// 치즈가 없고 방문한 부분은 temp에 -1로 저장
         temp[0][0] = -1;
 		while(!que.isEmpty()) {
 			int[] cur = que.poll();
@@ -52,11 +63,15 @@ public class BOJ_2638 {
 			for (int d = 0; d < 4; d++) {
 				int nr = r+dr[d];
 				int nc = c+dc[d];
+				// 범위 밖이거나 방문한 곳인 경우
 				if(nr<0 || nc<0 || nr>=R || nc>=C || temp[nr][nc]==-1) continue;
+				// 방문한 적 없고 치즈가 없는 부분일 경우
 				if(cheese[nr][nc]==0) {
+					// temp에 -1 저장해서 방문 표시하고 que에 넣기
                     temp[nr][nc] = -1;
                     que.offer(new int[] {nr, nc});
 				}
+				// 치즈가 있는 곳이면 temp에 닿아있는 면+1
 				if(cheese[nr][nc]==1) {
 					temp[nr][nc]++;
 				}
@@ -64,6 +79,7 @@ public class BOJ_2638 {
 		}
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
+				// 치즈가 바깥과 닿은 부분이 2 이상인 경우 녹음 => 치즈 크기-1
 				if(temp[i][j]>=2) {
 					cheese[i][j] = 0;
                     N--;
